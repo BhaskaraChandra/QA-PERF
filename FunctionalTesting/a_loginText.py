c94c8a8f933034fd1c33028f51c8ea2a14e7fe3e
Chandra.Bhaskara <chandra.bhaskara.s@gmail.com>
2025-06-03 22:11:36 +0530

    Fixed the Login Issue

--- FunctionalTesting/a_loginText.py
+++ FunctionalTesting/a_loginText.py
@@ -2,6 +2,8 @@
 import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 print("LoginTest")
 @pytest.fixture
 def driver():
@@ -14,6 +16,8 @@ def test_login(driver):
     driver.get("https://edupulse-qa.onrender.com/")
     driver.find_element("name","username").send_keys("sarthak@tiet.com")
     #driver.find_element("name","password").send_keys("a")
-    driver.find_element("name","Login").click()
+    #driver.find_element("name","Login").click()
+    driver.find_element(By.XPATH, "//button[text()='Login']").click()
+
     time.sleep(60)
-    assert driver.title == "Login Success"
+    assert driver.title == "User Dashboard"
